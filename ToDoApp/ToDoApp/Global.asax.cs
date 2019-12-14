1f766f886ef029ee866188cf7e89d754c85d58c3
aeleneiroxana <47813871+aeleneiroxana@users.noreply.github.com>
2019-12-14 12:36:51 +0200

    Added Code First Database

--- ToDoApp/ToDoApp/Global.asax.cs
+++ ToDoApp/ToDoApp/Global.asax.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using ToDoApp.Models;
 
 namespace ToDoApp
 {
@@ -12,6 +14,12 @@ namespace ToDoApp
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<UserDBContext>());
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<TaskDBContext>());
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<CommentDBContext>());
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<ProjectDBContext>());
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<TeamDBContext>());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
