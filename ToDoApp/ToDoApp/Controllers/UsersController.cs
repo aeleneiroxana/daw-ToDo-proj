64dd3276ea181d9387c2490bbeb533a4aae3b9ba
iancu360 <andrei@360advertising.ro>
2019-12-15 18:46:01 +0200

    users + teams + prohects

--- ToDoApp/ToDoApp/Controllers/UsersController.cs
+++ ToDoApp/ToDoApp/Controllers/UsersController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ToDoApp.Models;
 
 namespace ToDoApp.Controllers
 {
     public class UsersController : Controller
     {
+        private UserDBContext db = new UserDBContext();
+
         // GET: Users
         public ActionResult Index()
         {
+            var users = from user in db.Users
+                        orderby user.UserId
+                        select user;
+            ViewBag.Users = users;
             return View();
         }
     }
