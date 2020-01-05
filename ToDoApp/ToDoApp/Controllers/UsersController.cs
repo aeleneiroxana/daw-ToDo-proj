9e039cabeeba5173c3cb0816de0380ecb966fbfa
aeleneiroxana <47813871+aeleneiroxana@users.noreply.github.com>
2020-01-05 19:37:05 +0200

    Deleted from users 2 columns

--- ToDoApp/ToDoApp/Controllers/UsersController.cs
+++ ToDoApp/ToDoApp/Controllers/UsersController.cs
@@ -43,7 +43,6 @@ namespace ToDoApp.Controllers
                 if (TryUpdateModel(item))
                 {
                     item.Email = user.Email;
-                    item.PhoneNumber = user.PhoneNumber;
                     item.UserName = user.UserName;
                     try
                     {
